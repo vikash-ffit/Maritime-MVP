5dd1b3cd41bec7975de9b2399badae3f9537d788
vikash-ffit <vikash@flyingfeatherit.com>
2026-06-22 16:20:30 +0530

    Integrated prisma and connected it with postgres database

---
+++ src/lib/prisma.ts
@@ -0,0 +1,25 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
+};
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
